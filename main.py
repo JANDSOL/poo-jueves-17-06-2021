c1dcdeca56bcb451d9fce7bd049d77aada7af3c8
Juan Andrade <jpas.juan23@gmail.com>
2021-06-14 11:11:28 -0500

    Creation of variables in class LoopFor.

---
+++ main.py
@@ -0,0 +1,11 @@
+class LoopFor:
+
+    def __init__(self):
+        pass
+
+    def useFor(self):
+        name = "Daniel"
+        data = ["Daniel", 50, True]
+        numbers = (2, 5.6, 4, 1)
+        teacher = [{"nombre": "Erick", "final": 70}, {"nombre": "Yady", "final": 60}/
+                   {"nombre": "Danny", "final": 90}]
