71ede339e3b2a3f45bc3a36524e3b57cb7140e30
Juan Andrade <jpas.juan23@gmail.com>
2021-06-14 11:20:11 -0500

    Exercise 1 complete.

--- main.py
+++ main.py
@@ -7,5 +7,14 @@ class LoopFor:
         name = "Daniel"
         data = ["Daniel", 50, True]
         numbers = (2, 5.6, 4, 1)
-        teacher = [{"nombre": "Erick", "final": 70}, {"nombre": "Yady", "final": 60}/
+        teacher = [{"nombre": "Erick", "final": 70}, {"nombre": "Yady", "final": 60},\
                    {"nombre": "Danny", "final": 90}]
+        
+        # exercise 1
+        print("~ 1Exercise.")
+        for i in range(5):
+            print(" ! Número de i: {}".format(i))
+
+
+use_of_for = LoopFor()
+use_of_for.useFor()
